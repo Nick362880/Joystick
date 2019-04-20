8f1e9425853481adab07ae963fc79d283c0fed36
Nick G <nickelg25@gmail.com>
2019-04-20 12:14:03 -0400

    Fixed major mobile bug where multiple joysticks would align to incorrect touch coordinates in certain situations.

--- joystick.js
+++ joystick.js
@@ -1,66 +1,81 @@
-class JoyStick {
-	constructor(p) {
-		this.x = p.x;
-		this.y = p.y;
-		this.bc = p.bc;
-		this.kc = p.kc;
-		this.br = p.br;
-		this.kr = p.kr;
-		this.kx = 0;
-		this.ky = 0; // Knob coords
-		this.deltaX = 0;
-		this.deltaY = 0;
-		this.force = 0; // Can be used for dynamic velocity
-		this.canvas = this.newCanvas(p.br * 2, p.br * 2);
-		this.canvas.style.position = "absolute";
-		this.canvas.style.left = "" + this.x + "px";
-		this.canvas.style.top = "" + this.y + "px";
-		this.canvas.style.backgroundColor = this.rgba(this.bc);
-		this.canvas.style.borderRadius = "50%";
-		this.w = this.canvas.width;
-		this.h = this.canvas.height;
-		this.ctx = this.canvas.getContext("2d");
-		this.ctx.translate(this.w / 2, this.h / 2); // Moves origin to canvas center
-		
-		// Read why https://stackoverflow.com/questions/12731528/adding-event-listeners-in-constructor
-		var self = this;
-		// For mouse drag
-		var isdown = false;
-		this.addEventListeners(self.canvas, ["mouseup", "mouseout", "touchend"], function() { // Fix mouseout
-			isdown = false;
-			self.kx = 0;
-			self.ky = 0;
-			self.update();
-		});
-		this.addEventListeners(self.canvas, ["mousedown", "touchstart"], function(e) {
-			isdown = true;
-			if (e.touches) { // If touch (not mousedown)
-				self.kx = e.touches[0].clientX - self.x - self.w / 2;
-				self.ky = e.touches[0].clientY - self.y - self.h / 2;
-			} else {
-				self.kx = e.clientX - self.x - self.w / 2;
-				self.ky = e.clientY - self.y - self.h / 2;
-			}
+class Joystick {
+    constructor(x, y, br, kr, bc, kc) {
+        this.x = x || 0;
+        this.y = y || 0; // Top-left coords
+        this.br = br || 60;
+        this.kr = kr || 20; // Radius (background, knob)
+        this.bc = bc || "#777";
+        this.kc = kc || "#FFF";
+        this.kx = 0;
+        this.ky = 0; // Knob coords
+        this.deltaX = 0;
+        this.deltaY = 0;
+        this.force = 0; // Can be used for dynamic velocity
+        this.canvas = this.newCanvas(this.br * 2, this.br * 2);
+        this.canvas.style.position = "absolute";
+        this.canvas.style.left = "" + this.x + "px";
+        this.canvas.style.top = "" + this.y + "px";
+        this.canvas.style.backgroundColor = this.rgba(this.bc);
+        this.canvas.style.borderRadius = "50%";
+        this.w = this.canvas.width;
+        this.h = this.canvas.height;
+        this.ctx = this.canvas.getContext("2d");
+        this.ctx.translate(this.w / 2, this.h / 2); // Moves origin to canvas center
+        
+        // Read why https://stackoverflow.com/questions/12731528/adding-event-listeners-in-constructor
+        var self = this;
+        // For mouse drag
+        var isdown = false;
+        // Correct touch event (see bug log)
+        var touch;
+        var touchObj;
+        this.addEventListeners(self.canvas, ["mouseup", "mouseout", "touchend"], function() { // Fix mouseout
+            isdown = false;
+            self.kx = 0;
+            self.ky = 0;
+            self.update();
+        });
+        this.addEventListeners(self.canvas, ["mousedown", "touchstart"], function(e) {
+            isdown = true;
+            if (e.touches) { // If touch (not mousedown)
+                for (var i = 0; i < e.touches.length; i++) {
+                    if (e.touches[i].identifier == e.targetTouches[0].identifier) {
+                        touch = i;
+                    }
+                }
+                
+                self.kx = e.touches[touch].clientX - self.x - self.w / 2;
+                self.ky = e.touches[touch].clientY - self.y - self.h / 2;
+            } else {
+                self.kx = e.clientX - self.x - self.w / 2;
+                self.ky = e.clientY - self.y - self.h / 2;
+            }
             self.update();
         });
         this.addEventListeners(self.canvas, ["mousemove", "touchmove"], function(e) {
             if (e.touches) {
-				self.kx = e.touches[0].clientX - self.x - self.w / 2;
-				self.ky = e.touches[0].clientY - self.y - self.h / 2;
-			} else {
-				if (isdown) {
-					self.kx = e.clientX - self.x - self.w / 2;
-					self.ky = e.clientY - self.y - self.h / 2;
-				}
-			}
-			self.update();
+                for (var i = 0; i < e.touches.length; i++) {
+                    if (e.touches[i].identifier == e.targetTouches[0].identifier) {
+                        touch = i;
+                    }
+                }
+                
+                self.kx = e.touches[touch].clientX - self.x - self.w / 2;
+                self.ky = e.touches[touch].clientY - self.y - self.h / 2;
+            } else {
+                if (isdown) {
+                    self.kx = e.clientX - self.x - self.w / 2;
+                    self.ky = e.clientY - self.y - self.h / 2;
+                }
+            }
+            self.update();
         });
         
-		this.update();
-	}
-	
-	update() {
-		var a = Math.atan2(this.ky, this.kx);
+        this.update();
+    }
+    
+    update() {
+        var a = Math.atan2(this.ky, this.kx);
         var cos = Math.cos(a);
         var sin = Math.sin(a);
         if (this.kx && this.ky) { // If knob not at joystick center
@@ -71,52 +86,52 @@ class JoyStick {
             this.deltaY = 0;
             this.force = 0; // Cancelling deltas and force if joystick is inactive
         }
-		
-		var dk = this.dist(0, 0, this.kx, this.ky); // Origin - knob dist
-		var md = this.br - this.kr; // Maximum allowed dist
-		if (dk >= md) {
-			this.kx = cos * (this.br - this.kr);
-			this.ky = sin * (this.br - this.kr);
-		}
-		
-		dk = this.dist(0, 0, this.kx, this.ky); // Updating dk in case knob was moved
-		this.force = dk / md;
-		
-		var c = this.ctx;
-		c.clearRect(-this.w / 2, -this.h / 2, this.w, this.h);
-		c.beginPath();
-		c.arc(0, 0, this.br, 0, 2 * Math.PI);
-		c.fillStyle = this.rgba(this.bc);
-		c.fill();
-		
-		c.beginPath();
-		c.arc(this.kx, this.ky, this.kr, 0, 2 * Math.PI);
-		c.fillStyle = this.rgba(this.kc);
-		c.fill();
-	}
-	
-	// Some utility functions
-	newCanvas(ws, hs) {
-		var canv = document.createElement("canvas");
-		document.body.appendChild(canv);
-		canv.width = ws;
-		canv.height = hs;
-		return canv;
-	}
-	
-	rgba(arr) {
-		return "rgba(" + arr + ")";
-	}
-	
-	dist(x1, y1, x2, y2) {
-		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-	}
-	
-	// Like normal listener, but with multiple events (p)
-	addEventListeners(e, p, func) {
-		for (var i = 0; i < p.length; i++) {
-			var c = p[i];
-			e.addEventListener(c, func);
-		}
-	}
+        
+        var dk = this.dist(0, 0, this.kx, this.ky); // Origin - knob dist
+        var md = this.br - this.kr; // Maximum allowed dist
+        if (dk >= md) {
+            this.kx = cos * (this.br - this.kr);
+            this.ky = sin * (this.br - this.kr);
+        }
+        
+        dk = this.dist(0, 0, this.kx, this.ky); // Updating dk in case knob was moved
+        this.force = dk / md;
+        
+        var c = this.ctx;
+        c.clearRect(-this.w / 2, -this.h / 2, this.w, this.h);
+        c.beginPath();
+        c.arc(0, 0, this.br, 0, 2 * Math.PI);
+        c.fillStyle = this.rgba(this.bc); // FIX (not always rgba)
+        c.fill();
+        
+        c.beginPath();
+        c.arc(this.kx, this.ky, this.kr, 0, 2 * Math.PI);
+        c.fillStyle = this.rgba(this.kc); // FIX
+        c.fill();
+    }
+    
+    // Some utility functions
+    newCanvas(ws, hs) {
+        var canv = document.createElement("canvas");
+        document.body.appendChild(canv);
+        canv.width = ws;
+        canv.height = hs;
+        return canv;
+    }
+    
+    rgba(arr) {
+        return "rgba(" + arr + ")";
+    }
+    
+    dist(x1, y1, x2, y2) {
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    }
+    
+    // Like normal listener, but with multiple events (p)
+    addEventListeners(e, p, func) {
+        for (var i = 0; i < p.length; i++) {
+            var c = p[i];
+            e.addEventListener(c, func);
+        }
+    }
 }
