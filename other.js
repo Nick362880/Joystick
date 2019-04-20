324cac628325b22bcd5cafd1b0e7911841ed367f
Nick G <nickelg25@gmail.com>
2019-04-20 12:12:32 -0400

    Minor changes. Added additional joystick, primarily for use on mobile devices

--- other.js
+++ other.js
@@ -1,48 +1,42 @@
 var ctx, w, h;
-var j;
+var j1, j2;
 
 var x, y;
 var bx, by;
 
 window.onload = function() {
-	var canv = document.createElement("canvas");
-	document.body.appendChild(canv);
-	canv.width = w = window.innerWidth;
-	canv.height = h = window.innerHeight;
-	ctx = canv.getContext("2d");
-	
-	j = new JoyStick({
-		x: w / 2 - 80,
-		y: h - 40 - 80 * 2, // Coords (top left)
-		bc: [0, 0, 0, 0.5], // Background col
-		kc: [255, 255, 255, 0.75], // Knob col
-		br: 80, // Background radius
-		kr: 20, // Knob radius
-	});
-	
-	x = w / 2 - 10;
-	y = h / 2 - 10;
-	bx = 0;
-    by = 0;
-	
-	window.requestAnimationFrame(main);
+    var canv = document.createElement("canvas");
+    document.body.appendChild(canv);
+    canv.width = w = window.innerWidth;
+    canv.height = h = window.innerHeight;
+    ctx = canv.getContext("2d");
+    
+    j1 = new Joystick(w / 2 - 48 * 3, h - 40 - 48 * 2, 48, 16, [101, 101, 101, 0.5], [255, 255, 255, 0.75]);
+    
+    j2 = new Joystick(w / 2 + 48, h - 40 - 48 * 2, 48, 16, [101, 101, 101, 0.5], [255, 255, 255, 0.75]);
+    
+    x = w / 2 - 20;
+    y = h / 2;
+    bx = w / 2 + 20;
+    by = h / 2;
+    
+    window.requestAnimationFrame(main);
 }
 
 function main() {
-	x += j.deltaX * j.force * 4;
-    y += j.deltaY * j.force * 4;
-    //ctx.fillStyle = "rgba(0, 0, 0, 0.1)"
-    //ctx.fillRect(0, 0, w, h);
-    ctx.clearRect(0, 0, w, h);
+    x += j1.deltaX * j1.force * 4;
+    y += j1.deltaY * j1.force * 4;
+    bx += j2.deltaX * j2.force * 4;
+    by += j2.deltaY * j2.force * 4;
+    
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, w, h);
+    // ctx.clearRect(0, 0, w, h);
     
     ctx.beginPath();
     ctx.fillStyle = "#C00"
     ctx.arc(x, y, 10, 0, 2 * Math.PI);
     ctx.fill();
-    
-    var a = Math.atan2(y - by, x - bx);
-    bx += Math.cos(a);
-    by += Math.sin(a);
     
     ctx.beginPath();
     ctx.fillStyle = "#099";
